8bc43d03dfa7da5062eb7233035c8da8ffb0e9f8
Hiroki Takanashi <takanashi1986@gmail.com>
2021-02-22 01:44:57 +0900

    fmt

--- data.tf
+++ data.tf
@@ -19,7 +19,7 @@ locals {
     data.terraform_remote_state.global.outputs.production_operation_server
     : data.terraform_remote_state.global.outputs.development_operation_server
   )
-  ecr_csweb_app          = data.terraform_remote_state.global.outputs.ecr_csweb_app
+  ecr_csweb_app       = data.terraform_remote_state.global.outputs.ecr_csweb_app
   vpc_id              = local.network.vpc_id
   public_subnet_ids   = local.network.public_subnet_ids
   webapp_subnet_ids   = local.network.webapp_subnet_ids
--- main.tf
+++ main.tf
@@ -1,42 +1,42 @@
 module "security_group" {
-  source   = "./modules/security_group"
-  env      = var.env
-  project  = var.project
-  vpc_id   = local.vpc_id
-  rds_port = 3306
-  webapp_subnet_ids      = local.webapp_subnet_ids
+  source            = "./modules/security_group"
+  env               = var.env
+  project           = var.project
+  vpc_id            = local.vpc_id
+  rds_port          = 3306
+  webapp_subnet_ids = local.webapp_subnet_ids
 }
 
 module "log_group" {
-  source   = "./modules/log_group"
-  env      = var.env
-  project  = var.project
+  source  = "./modules/log_group"
+  env     = var.env
+  project = var.project
 }
 
 module "alb" {
   source = "./modules/alb"
 
-  env                    = var.env
-  project                = var.project
-  hosted_zone_id         = var.hosted_zone_id
-  hosted_zone_name       = var.hosted_zone_name
-  vpc_id   = local.vpc_id
-  subdomain_csweb = var.subdomain_csweb
-  csweb_app_health_check_path      = var.csweb_app_health_check_path
-  public_subnet_ids      = local.public_subnet_ids
-  sg_alb_csweb_ids    = [module.security_group.alb_csweb_id]
+  env                         = var.env
+  project                     = var.project
+  hosted_zone_id              = var.hosted_zone_id
+  hosted_zone_name            = var.hosted_zone_name
+  vpc_id                      = local.vpc_id
+  subdomain_csweb             = var.subdomain_csweb
+  csweb_app_health_check_path = var.csweb_app_health_check_path
+  public_subnet_ids           = local.public_subnet_ids
+  sg_alb_csweb_ids            = [module.security_group.alb_csweb_id]
 }
 
 module "ecs" {
   source = "./modules/ecs"
 
-  env                    = var.env
-  project                = var.project
-  webapp_subnet_ids      = local.webapp_subnet_ids
-  sg_ecs_csweb_ids          = [module.security_group.ecs_csweb_id]
-  csweb_app_repository_url  = local.ecr_csweb_app.repository_url
+  env                        = var.env
+  project                    = var.project
+  webapp_subnet_ids          = local.webapp_subnet_ids
+  sg_ecs_csweb_ids           = [module.security_group.ecs_csweb_id]
+  csweb_app_repository_url   = local.ecr_csweb_app.repository_url
   alb_target_group_csweb_arn = module.alb.target_group_csweb_arn
-  log_group_ecs_csweb_app   = module.log_group.ecs_csweb_app
+  log_group_ecs_csweb_app    = module.log_group.ecs_csweb_app
 }
 
 module "rds" {

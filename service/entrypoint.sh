29c672744b3c04f4330125561c3151ae90142f78
Hiroki Takanashi <takanashi1986@gmail.com>
2021-02-27 15:23:52 +0900

    moved tf files under ./service

--- service/entrypoint.sh
+++ service/entrypoint.sh
@@ -2,14 +2,14 @@ if [ -z ${ENV} ]; then
   echo "ENV must be specified like 'ENV=dev make init'"
   exit 1
 fi
-BACKEND_ENV_FILE=env/${ENV}/backend.env
+BACKEND_ENV_FILE=../env/${ENV}/backend.env
 BACKEND_ENVS=$(cat ${BACKEND_ENV_FILE} | grep -v '\#' | xargs)
 BE_BUCKET=$(env ${BACKEND_ENVS} sh -c 'echo $BE_BUCKET')
 BE_KEY=$(env ${BACKEND_ENVS} sh -c 'echo $BE_KEY')
 BE_REGION=$(env ${BACKEND_ENVS} sh -c 'echo $BE_REGION')
 
 
-GLOBAL_BACKEND_ENV_FILE=env/global/backend.env
+GLOBAL_BACKEND_ENV_FILE=../env/global/backend.env
 GLOBAL_ENVS=$(cat ${GLOBAL_BACKEND_ENV_FILE} | grep -v '\#' | xargs)
 GLBOAL_BE_BUCKET=$(env ${GLOBAL_ENVS} sh -c 'echo $BE_BUCKET')
 GLOBAL_BE_KEY=$(env ${GLOBAL_ENVS} sh -c 'echo $BE_KEY')
